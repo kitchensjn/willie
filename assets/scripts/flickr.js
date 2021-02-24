524b616f436151059c9ea489847cc2fb5c84083d
James Kitchens <40303683+kitchensjn@users.noreply.github.com>
2021-02-24 13:27:01 -0800

    homepage mobile friendly

--- assets/scripts/flickr.js
+++ assets/scripts/flickr.js
@@ -1,1 +1,39 @@
-fetch("http://api.flickr.com/services/feeds/groups_discuss.gne?id=34427469792@N01&lang=en-us&format=rss_200");
+function JavaScriptFetch() {
+    var script = document.createElement('script');
+    script.src = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&id=26424453@N08&tags=" + document.getElementById("search").value;
+    document.querySelector('head').appendChild(script);
+}
+
+function jsonFlickrFeed(data) {
+    var image = "";
+    data.items.forEach(function (element) {
+        image += "<img src=\"" + element.media.m + "\"/>";
+    });
+    document.getElementById("outputDiv").innerHTML = image;
+}
+
+
+
+/*
+var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?&id=26424453@N08";
+
+function getPhotos() {
+    $.getJSON(flickerAPI, {
+        tags: $("#search").val(),
+        tagmode: "any",
+        format: "json"
+    }).done(function (result, status, xhr) {
+        $.each(result.items, function (i, item) {
+            $("<img>").attr("src", item.media.m).appendTo("#outputDiv");
+        });
+    }).fail(function (xhr, status, error) {
+        alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+    });
+}
+
+getPhotos();
+
+$('#submit').click(function() {
+    getPhotos();
+});
+*/
